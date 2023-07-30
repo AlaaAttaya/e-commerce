8f59d2cec3089490eb3b0c93b3981f8d86e823a9
Alaa Attaya <attayaalaa@gmail.com>
2023-07-31 02:32:09 +0300

    Javascript for add/edit image & search onkeyup

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -27,3 +27,98 @@ for (var i = 0; i < editproduct.length; i++) {
     
     });
 }
+
+
+//picture-add
+const imagePreviewLabel = document.getElementById('imagePreviewLabel');
+imagePreviewLabel.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  document.getElementById('imageInput').click();
+});
+
+document.getElementById('imageInput').addEventListener('change', function(event) {
+    const fileInput = event.target;
+    const imagePreview = document.getElementById('imagePreview');
+  
+    if (fileInput.files && fileInput.files[0]) {
+        const reader = new FileReader();
+  
+        reader.onload = function(e) {
+            imagePreview.style.backgroundImage = `url('${e.target.result}')`;
+        };
+  
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+  });
+
+
+
+  //ADD an item//
+  //const fileInput = document.getElementById("imageInput");
+    // if (fileInput.files.length > 0) {
+    // const file = fileInput.files[0]; 
+
+    // console.log("File name:", file.name);
+    // console.log("File type:", file.type);
+    // console.log("File size (in bytes):", file.size);
+
+    
+
+      
+    //   if (fileInput.files.length === 0) {
+       
+    //     return;
+    //   }
+    
+      
+    //   const formData = new FormData();
+    
+      
+    //   formData.append("file", fileInput.files[0]);
+
+
+
+
+
+
+
+
+//picture-edit
+const imagePreviewLabelEdit = document.getElementById('imagePreviewLabel-edit');
+imagePreviewLabelEdit.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  document.getElementById('imageInput-edit').click();
+});
+
+document.getElementById('imageInput-edit').addEventListener('change', function(event) {
+    const fileInput2 = event.target;
+    const imagePreview = document.getElementById('imagePreview-edit');
+  
+    if (fileInput2.files && fileInput2.files[0]) {
+        const reader = new FileReader();
+  
+        reader.onload = function(e) {
+            imagePreview.style.backgroundImage = `url('${e.target.result}')`;
+        };
+  
+        reader.readAsDataURL(fileInput2.files[0]);
+    }
+  });
+
+
+
+
+
+
+
+  //Search-onkeyup
+  
+    const myInput = document.getElementById('search-onkeyup');
+    myInput.addEventListener('keyup', function() {
+    let keyupvalue = myInput.value;
+    console.log(keyupvalue);
+    
+
+   
+   
+    });
